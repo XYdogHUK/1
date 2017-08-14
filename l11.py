182821773853fe5d6a44e6e6aba80a330139dbd5
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-14 20:19:00 +0300

    new6

--- l11.py
+++ l11.py
@@ -1,3 +1,6 @@
-a = 100
-b = 200
-print(a < b, a >= b, a == 100, b != 199)
+a = float(input(" Bвeдитe первое число : "))
+b = float(input(" Bвeдитe второе число : "))
+value_1 = " Пepвoe число больше второго . "
+value_2 = " Второе число не меньше первого . "
+res = value_1 if a>b else value_2
+print(res)
