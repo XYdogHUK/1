f8dfde4324615f793449ab26d88e4ae575b8a959
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 10:38:48 +0300

    new9

--- l11.py
+++ l11.py
@@ -2,8 +2,8 @@ res = eval(input('Введите что-нибудь:'))
 resType = type(res)
 if resType == int:
     print('Это целое число')
-if resType == float:
-    print("Это действительное число")
-if resType != int and resType != float:
-    print("Тут точно не цифры")
-print("Всё, отстань")
+elif resType == float:
+    print("Это действительное число ")
+else:
+    print("Наверное это текст")
+print("Работа завершена")
