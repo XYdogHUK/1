ae46dea0e31c50fca0455f3102f20a21bbcb336d
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 11:57:01 +0300

    new18

--- l11.py
+++ l11.py
@@ -3,7 +3,9 @@ try:
     a = float(input("Введите a:"))
     b = float(input("Введите b:"))
     x = b / a
-    print("решение уравнения: x=",x)
-except:
-    print("Вы ввели некорректные данные")
+    print("решение уравнения: x=", x)
+except ValueError:
+    print("Нужно было ввести число")
+except ZeroDivisionError:
+    print("Внимание! На ноль делить нельзя")
 print("the end")
