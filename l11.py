6c9bf4b4d1508251fb20300dde1639281426a36d
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 11:51:03 +0300

    new17

--- l11.py
+++ l11.py
@@ -1,17 +1,9 @@
-print("поиск совпадающих элементов")
-A=[2,False,9.1,2-1j,"hello",5,"Python"]
-B=[5,3,"HELLO",7,12.5,"Python",True,False]
-print("1-й список:",A)
-print("2-й список:",B)
-print("Совпадают:")
-i=0
-for a in A:
-    i+=1
-    j=0
-    for b in B:
-        j+=1
-        if a==b:
-            txt=str(i)+"-й элемент из 1-ого списка и "
-            txt=txt+str(j)+"-й элемент из 2-ого списка"
-            print(txt)
-print("Поиск закончен")
+print("ax=b")
+try:
+    a = float(input("Введите a:"))
+    b = float(input("Введите b:"))
+    x = b / a
+    print("решение уравнения: x=",x)
+except:
+    print("Вы ввели некорректные данные")
+print("the end")
