e605f86e4df92f055ee674327aed2af2314fbce5
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 10:57:37 +0300

    new13

--- l11.py
+++ l11.py
@@ -1,15 +1,7 @@
-n = 500
-dz = 1 / n;
-pts = 0
-i = 0
-while i <= n:
-    x = dz * i
-    j = 0
-    while j <= n:
-        y = dz * j
-        if y <= x and y >= x ** 2:
-            pts = pts + 1
-        j += 1
-    i += 1
-s = pts / (n + 1) ** 2
-print("Площадь фигуры:",s)
+print("bla bla")
+n = 100
+text = "1+2+...+" + str(n) + " ="
+s=0
+for i in range(1,n+1):
+    s+=i
+print(text,s)
