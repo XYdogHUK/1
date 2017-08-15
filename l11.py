251a50ddccfb33fdc8f52444bb8f8897f20adef9
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 11:47:00 +0300

    new16

--- l11.py
+++ l11.py
@@ -1,10 +1,17 @@
-print("проверяем список")
-myList=[1,3+2j,True,4.0]
-print("Список:",myList)
-for s in myList:
-    if type(s)==str:
-        print("В списке есть текстовые элементы")
-        break
-else:
-    print("В списке нет текстовых элементов")
-print("Конец")
+print("поиск совпадающих элементов")
+A=[2,False,9.1,2-1j,"hello",5,"Python"]
+B=[5,3,"HELLO",7,12.5,"Python",True,False]
+print("1-й список:",A)
+print("2-й список:",B)
+print("Совпадают:")
+i=0
+for a in A:
+    i+=1
+    j=0
+    for b in B:
+        j+=1
+        if a==b:
+            txt=str(i)+"-й элемент из 1-ого списка и "
+            txt=txt+str(j)+"-й элемент из 2-ого списка"
+            print(txt)
+print("Поиск закончен")
