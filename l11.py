81e0f02d9cf47152d51957b14b1f9874995061fb
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-08-15 10:54:37 +0300

    new12

--- l11.py
+++ l11.py
@@ -1,11 +1,15 @@
-print("Сумма натуральных чисел")
-n = 100
-text = "1+2+...+" + str(n) + " ="
-i = 1
-s = 0
-while True:
-    s += i
+n = 500
+dz = 1 / n;
+pts = 0
+i = 0
+while i <= n:
+    x = dz * i
+    j = 0
+    while j <= n:
+        y = dz * j
+        if y <= x and y >= x ** 2:
+            pts = pts + 1
+        j += 1
     i += 1
-    if i > n:
-        break
-print(text, s)
+s = pts / (n + 1) ** 2
+print("Площадь фигуры:",s)
