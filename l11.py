705a136481bc24a2c3ec12dec79d08af59f6f383
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-09-02 12:27:23 +0300

    13?

--- l11.py
+++ l11.py
@@ -1,4 +1,12 @@
+x = 100
+
+
 def test_vars():
-    print("В теле функции x=",x)
-x="глобпер"
-test_vars()
+    global x, y
+    print("В теле функ x=", x)
+    y = 200
+    print("В теле функ y=", y)
+    x = 300
+    test_vars()
+    print("Вне фун x=", x)
+    print("Вне фун y=", y)
