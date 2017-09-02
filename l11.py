9be85bd6514c3aa671135b42ec77a554234ceadd
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-09-02 11:25:23 +0300

    xx

--- l11.py
+++ l11.py
@@ -1,12 +1,20 @@
-def my_exp(x, n):
-    s = 0
-    q = 1
-    for k in range(n + 1):
-        s += q
-        q *= x / (k + 1)
-    return s
+def print_text(txt="Значение аргумента по умолчанию."):
+    print(txt)
 
 
-x = 1
-for n in range(11):
-    print("n =", n, "->", my_exp(x, n))
+def show_args(a, b="Второй аргумент не указан."):
+    print(a, b)
+
+
+def my_func(x="1-й аргумент x.", y="2-й аргумент y.", ):
+    print(x, y)
+
+
+print_text("Явно указан.")
+print_text()
+show_args("Первый.", "Второй")
+show_args("Первый.")
+my_func()
+my_func("Один из аргументовю")
+my_func(y="Один из аргументовю")
+
