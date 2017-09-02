58720a7fc6b60644c7d8638d3f391246949cd569
XYdogHUK <30670061+XYdogHUK@users.noreply.github.com>
2017-09-02 12:05:00 +0300

    66

--- l11.py
+++ l11.py
@@ -1,29 +1,8 @@
-import math
-
-
-def solve_deqn(f, x0, y0, x):
-    n = 1000
-    dx = (x - x0) / n
-    x = x0
-    y = y0
-    for k in range(1, n + 1):
-        y = y + dx * f(x, y)
-        x = x + dx
-    return y
-
-
-def diff_eqn(x, y):
-    return 2 * x - y
-
-
-def y(x):
-    return 2 * (x - 1) + 5 * math.exp(-x)
-
-
-h = 0.5
-for k in range(0, 6):
-    x = k * h
-    print("Числовое решение:")
-    print("x=", x, "-> y(x)=", solve_deqn(diff_eqn, 0, 3, x))
-    print("Точное решение:")
-    print("x=", x, "-> y(x)=", y(x))
+def Fib(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return Fib(n-1)+Fib(n-2)
+print("Числа Фибоначчи:")
+for i in range(1,16):
+    print(Fib(i),end="")
